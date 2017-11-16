7fc978cf58f5701791156e08819872b7a7d5caae
Melvin Patterson <mel-mbp17@Mel-MBP17-2.local>
2017-11-15 18:58:01 -0800

    Added main view and started working on makelist

--- ShopShopShop/MakeList/MakeListVC.swift
+++ ShopShopShop/MakeList/MakeListVC.swift
@@ -8,18 +8,55 @@
 
 import UIKit
 
-class MakeListVC: UIViewController {
-
+class MakeListVC: UIViewController { //it's not a table view controller, it's a view controller with a table view inside of it
+    
+    var tableData = ["Delicious Apple", "Granny Smith", "Fuji Apple", "California Apple"]
+    //test to
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?){
+        if segue.identifier == "StoreListSegue" {
+            let dest = segue.destination as! StoreListVC
+            //            let dest.shoppingList = tableData  comment out for now, but I do need this
+        }
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
+
+extension MakeListVC: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return tableData.count
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "listItemsTableViewCell")!
+        let oneitem = tableData[indexPath.row]
+        cell.textLabel?.text = oneitem
+        return cell
+        
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
